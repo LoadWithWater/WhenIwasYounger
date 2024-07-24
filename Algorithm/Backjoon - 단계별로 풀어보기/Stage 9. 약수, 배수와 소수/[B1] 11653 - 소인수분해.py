33b36e0c3a81d99ed054b618df1386b060981ff6
LoadWithWater <rlarhkdwo98@naver.com>
2024-07-24 22:25:05 +0900

    BJ Stage 9

--- Algorithm/Backjoon - 단계별로 풀어보기/Stage 9. 약수, 배수와 소수/[B1] 11653 - 소인수분해.py
+++ Algorithm/Backjoon - 단계별로 풀어보기/Stage 9. 약수, 배수와 소수/[B1] 11653 - 소인수분해.py
@@ -1,14 +1,19 @@
-list = []
-
 def prime(N):
     temp = N
 
     if (N == 1):
-        return None
+        return ""
+    elif (N == 2):
+        print(2)
+    elif (N == 3):
+        print(3)
     else:
-        return None
-
+        for i in range(2, N):
+            while (temp % i == 0):
+                print(i)
+                temp = temp / i
+                if (temp == 1):
+                    break
 
 N = int(input())
 prime(N)
-print(list)
